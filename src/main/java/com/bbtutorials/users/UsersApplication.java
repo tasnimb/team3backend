ab8b41631d21907a5a904ec2162403b16a392347
has992 <89402453+has992@users.noreply.github.com>
2021-10-26 23:38:40 +0100

    adding jenkins/tomcat stuff

--- src/main/java/com/bbtutorials/users/UsersApplication.java
+++ src/main/java/com/bbtutorials/users/UsersApplication.java
@@ -3,11 +3,17 @@ package com.bbtutorials.users;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+
 @SpringBootApplication
-public class UsersApplication {
+public class UsersApplication extends SpringBootServletInitializer {
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(UsersApplication.class);
+    }
 
 	public static void main(String[] args) {
 		SpringApplication.run(UsersApplication.class, args);
 	}
-
 }
